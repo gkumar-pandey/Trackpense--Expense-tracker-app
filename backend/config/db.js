80f66a98cce4e11ffa9e40cbceb9db55e6967c42
gkumar-pandey <gautamshekhar078@gmail.com>
2024-02-19 17:56:35 +0530

    feat: connect with database

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,9 @@
 const mongoose = require("mongoose");
 
 const connectDb = async () => {
-  const DB_URL = "";
+  const DB_USER = process.env.DB_USER;
+  const DB_PASSWORD = process.env.DB_PASSWORD;
+  const DB_URL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.2jzodab.mongodb.net/?retryWrites=true&w=majority`;
   try {
     const connect = await mongoose.connect(DB_URL);
     console.log("Database connected successfully.");
